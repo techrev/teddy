6d031d02bde473cdb414290daa7c3ff7aeb0af76
Jill Stellman <techgirl@mail.com>
2018-11-30 11:32:03 -0500

    Starting Karma "manually" in the travis.yml.

--- karma.conf.js
+++ karma.conf.js
@@ -55,7 +55,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: false,
     singleRun: true,
-    browsers: ['Firefox'],
+    browsers: ['Chrome_Headless', 'Firefox'],
     concurrency: 1
   }
 
