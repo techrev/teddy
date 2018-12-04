824775745e37d706caa395216612ef4a1af856ad
Jill Stellman <jill.stellman@caci.com>
2018-12-04 17:27:03 -0500

    Add script to download chrome 67 that is supposed to work with Windows 2016.

---
+++ downloadChrome.ps1
@@ -0,0 +1,6 @@
+﻿$url = "http://www.slimjetbrowser.com/chrome/win/chrome64_67.0.3396.79.exe"
+$path = "chrome64_67.0.3396.79.exe"
+
+$path = Join-Path $pwd $path
+"Downloading [$url]`nSaving at [$path]"
+Invoke-WebRequest -Uri $url -OutFile $path
