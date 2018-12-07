6ad52f83c9874bf474a851ea0dfb0503b427fc24
Jill Stellman <techgirl@mail.com>
2018-12-07 10:25:48 -0500

    Trying to get the ps1 file to actually find the executable.

--- installChrome.ps1
+++ installChrome.ps1
@@ -1,12 +1,19 @@
 $exeName = "chrome64_67.0.3396.79.exe"
+$origPath = $pwd
 $fileDetails = Get-ChildItem $pwd | findstr -i $exeName
 "Chrome installer executable is $fileDetails`n"
 if ($fileDetails -ne $null) {
     "Starting chrome installer`n"
     Start-Process $exeName -ArgumentList "silent", "install"
     if ($? -eq $true) {
-        "Install complete"
-        
+        "Install complete`nChecking information after install:"
+        $exePath = "C:\Program Files (x86)\Google\Chrome\Application"
+        $exeName = "chrome.exe"
+        Set-Location -Path $exePath
+        "working directory is $pwd"
+        $chromeLoc = Get-ChildItem $exePath | findstr -i "chrome.exe"
+        "chrome exe is $chromeLoc"
+        Set-Location -Path $origPath
     } else {
         "Install did not complete properly"
     }
--- startChrome.ps1
+++ startChrome.ps1
@@ -1,6 +1,15 @@
 "Starting chrome."
-$exeName = "/c/Program Files (x86)/Google/Chrome/Application/chrome.exe"
-Start-Process -FilePath $exeName -ArgumentList "--headless", "remote-debugging-port=9333", "--enable-logging"
+$origPath = $pwd
+$exePath = "C:\Program Files (x86)\Google\Chrome\Application"
+$exeName = "chrome.exe"
+Set-Location -Path $exePath
+"working directory is $pwd"
+$chromeLoc = Get-ChildItem $exePath | findstr -i "chrome.exe"
+"chrome executable is $chromeLoc"
+$path = Join-Path $exePath $exeName
+"path is $path"
+Start-Process -FilePath $path -ArgumentList "--headless", "remote-debugging-port=9333", "--enable-logging"
+Set-Location -Path $origPath
 if ($? -ne $true) {
     "Could not start chrome"
 }
