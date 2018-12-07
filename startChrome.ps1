3e5f4e0a6e156893b9b63081866561ace23c6f56
Jill Stellman <techgirl@mail.com>
2018-12-07 10:04:23 -0500

    Still not finding the chrome executable in the ps1 file, so trying to change the path to match what the VM seems to like.

--- startChrome.ps1
+++ startChrome.ps1
@@ -1,5 +1,5 @@
 "Starting chrome."
-$exeName = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+$exeName = "/c/Program Files (x86)/Google/Chrome/Application/chrome.exe"
 Start-Process -FilePath $exeName -ArgumentList "--headless", "remote-debugging-port=9333", "--enable-logging"
 if ($? -ne $true) {
     "Could not start chrome"
