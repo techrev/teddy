77ff3a88bb592b954db5290a0717cfe0155df63c
Jill Stellman <techgirl@mail.com>
2018-12-07 08:48:20 -0500

    Installing from the .ps1 file isn't working. Changing to download via downloadInstallChrome.ps1, then install via the "command line" (yml file), then start from startChrome.ps1, and stop the processes via stopProcesses.ps1.

--- downloadInstallChrome.ps1
+++ downloadInstallChrome.ps1
@@ -5,19 +5,19 @@ $path = Join-Path $pwd $exeName
 "Downloading [$url]`nSaving at [$path]`n"
 Invoke-WebRequest -Uri $url -OutFile $path
 "Download complete`n"
-$fileDetails = Get-ChildItem $pwd | findstr -i $exeName
-"Chrome installer executable is $fileDetails`n"
-if ($fileDetails -ne $null) {
-    "Starting chrome installer`n"
-    Start-Process $exeName -ArgumentList "silent", "install"
-    if ($? -eq $true) {
-        Set-Location -Path "C:"
-        $chromeLoc = Get-ChildItem $pwd | findstr -i "chrome.exe"
-        "chrome executable location is $chromeLoc"
-        "Install complete"
-    } else {
-        "Install did not complete properly"
-    }
-} else {
-    "Could not find chrome installer in $pwd"
-}
+# $fileDetails = Get-ChildItem $pwd | findstr -i $exeName
+# "Chrome installer executable is $fileDetails`n"
+# if ($fileDetails -ne $null) {
+#     "Starting chrome installer`n"
+#     Start-Process $exeName -ArgumentList "silent", "install"
+#     if ($? -eq $true) {
+#         Set-Location -Path "C:"
+#         $chromeLoc = Get-ChildItem $pwd | findstr -i "chrome.exe"
+#         "chrome executable location is $chromeLoc"
+#         "Install complete"
+#     } else {
+#         "Install did not complete properly"
+#     }
+# } else {
+#     "Could not find chrome installer in $pwd"
+# }
