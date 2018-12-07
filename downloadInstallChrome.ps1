fe3007b03b47238ff6e8776c7d29c127cca8c77a
Jill Stellman <techgirl@mail.com>
2018-12-07 07:57:17 -0500

    Add some information statements fo downloadInstallChrome.ps1 and move the call to it in travis.yml

--- downloadInstallChrome.ps1
+++ downloadInstallChrome.ps1
@@ -2,11 +2,13 @@
 $path = "chrome64_67.0.3396.79.exe"
 
 $path = Join-Path $pwd $path
-"Downloading [$url]`nSaving at [$path]"
+"Downloading [$url]`nSaving at [$path]`n"
 Invoke-WebRequest -Uri $url -OutFile $path
-"Download complete`nStarting chrome installer"
+"Download complete`n"
 $fileDetails = Get-ChildItem $pwd | findstr -i chrome64-67.0.3396.79.exe
+"Chrome installer executable is $fileDetails`n"
 if ($fileDetails -ne $null) {
+    "Starting chrome installer`n"
     Start-Process "chrome64-67.0.3393.79.exe" -ArgumentList "silent", "install"
     if ($? -eq $true) {
         "Install complete"
