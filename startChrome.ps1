bfcf686daa7091d07017487f98e53cff0b12ef04
Jill Stellman <techgirl@mail.com>
2018-12-10 08:14:57 -0500

    Removing directory switching to see if call to start chrome will work without it.

--- startChrome.ps1
+++ startChrome.ps1
@@ -1,15 +1,11 @@
 "Starting chrome."
-$origPath = $pwd
+# $origPath = $pwd
 $exePath = "C:\Program Files (x86)\Google\Chrome\Application"
 $exeName = "chrome.exe"
-Set-Location -Path $exePath
-"working directory is $pwd"
-$chromeLoc = Get-ChildItem $exePath | findstr -i "chrome.exe"
-"chrome executable is $chromeLoc"
+# Set-Location -Path $exePath
 $path = Join-Path $exePath $exeName
-"path is $path"
 Start-Process -FilePath $path -ArgumentList "--headless", "remote-debugging-port=9333", "--enable-logging"
-Set-Location -Path $origPath
+# Set-Location -Path $origPath
 if ($? -ne $true) {
     "Could not start chrome"
 }
