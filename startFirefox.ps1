a9f77cdec5ab1cf4ec74aa2a5733bfb886997098
Jill Stellman <techgirl@mail.com>
2018-12-13 18:18:09 -0500

    Added code to startFirefox to find paths and such.

--- startFirefox.ps1
+++ startFirefox.ps1
@@ -1,25 +1,29 @@
 $nodePath = "C:\ProgramData\nvs\node\11.4.0\x64"
 $nvsDir = Get-ChildItem -Path $nodePath
 "nvs directory listing is $nvsDir"
-"Starting karma server"
+# "Starting karma server"
 $path = Join-Path $nodePath "node.exe"
-Start-Process -FilePath $path -WorkingDirectory $nodePath -ArgumentList "karma", "start"
-"Sleeping for 30 seconds to let karma server get fully started"
-Start-Sleep -s 30
-$karmaProcId = Get-Process -Name "node" | select -expand id
-"karmaProcId is $karmaProcId"
-if ($karmaProcId) {
-    "Starting firefox"
-    $exePath = "C:\Program Files\Mozilla Firefox"
-    $exeName = "firefox.exe"
-    $path = Join-Path $exePath $exeName
-    Start-Process -FilePath $path -ArgumentList "http://localhost:8000" "-headless"
-    if ($? -ne $true) {
-        "Could not start firefox"
-    }
-    $procId = Get-Process -Name "firefox" | select -expand id
-    "firefox PID is $procId"
-} else {
-    "Karma did not start. Did not start firefox."
-}
+"path to node is $path"
+"working directory is $pwd"
+$karmaFFPath = Join-Path $pwd "node_modules\karma-firefox-launcher"
+$karmaFFDir = Get-ChildItem -Path $karmaFFPath
+# Start-Process -FilePath $path -WorkingDirectory $nodePath -ArgumentList "karma", "start"
+# "Sleeping for 30 seconds to let karma server get fully started"
+# Start-Sleep -s 30
+# $karmaProcId = Get-Process -Name "node" | select -expand id
+# "karmaProcId is $karmaProcId"
+# if ($karmaProcId) {
+#     "Starting firefox"
+#     $exePath = "C:\Program Files\Mozilla Firefox"
+#     $exeName = "firefox.exe"
+#     $path = Join-Path $exePath $exeName
+#     Start-Process -FilePath $path -ArgumentList "http://localhost:8000" "-headless"
+#     if ($? -ne $true) {
+#         "Could not start firefox"
+#     }
+#     $procId = Get-Process -Name "firefox" | select -expand id
+#     "firefox PID is $procId"
+# } else {
+#     "Karma did not start. Did not start firefox."
+# }
 
