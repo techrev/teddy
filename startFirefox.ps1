e629b51069f8bda1a02d853f634b3ab6dc67a66f
Jill Stellman <techgirl@mail.com>
2018-12-13 13:25:45 -0500

    Added sleep time to allow karma server to get started fully and check for process id to make sure it's running.

--- startFirefox.ps1
+++ startFirefox.ps1
@@ -1,8 +1,10 @@
 "Starting karma server"
 $path = Join-Path $pwd "karma"
 Start-Process -FilePath $pwd -ArgumentList "start"
-$karmaStarted = $? -eq $true
-"Karma started? $karmaStarted"
+"Sleeping for 30 seconds to let karma server get fully started"
+Start-Sleep -s 30
+$karmaProcId = Get-Process -Name "node" | select -expand id
+"karmaProcId is $karmaProcId"
 "Starting firefox"
 $exePath = "C:\Program Files\Mozilla Firefox"
 $exeName = "firefox.exe"
