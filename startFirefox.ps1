1a3791175fd38edc6ce6490ba894cb6807c3e1dc
Jill Stellman <techgirl@mail.com>
2018-12-13 11:33:36 -0500

    Starting karma server only, then starting firefox trying to connect to karma server.

--- startFirefox.ps1
+++ startFirefox.ps1
@@ -2,7 +2,7 @@
 $exePath = "C:\Program Files\Mozilla Firefox"
 $exeName = "firefox.exe"
 $path = Join-Path $exePath $exeName
-Start-Process -FilePath $path -ArgumentList "-headless"
+Start-Process -FilePath $path -ArgumentList "http://localhost:8000" "-headless"
 if ($? -ne $true) {
     "Could not start firefox"
 }
