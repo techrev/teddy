b646b1c571ff89365fe3e6e4d8ecd1e974b31610
Jill Stellman <techgirl@mail.com>
2018-12-13 14:51:35 -0500

    Changed powershell script to start a node process (karma).

--- startFirefox.ps1
+++ startFirefox.ps1
@@ -1,17 +1,25 @@
+$nodePath = "C:\ProgramData\nvs\node\11.4.0\x64"
+$nvsDir = Get-ChildItem -Path $nodePath
+"nvs directory listing is $nvsDir"
 "Starting karma server"
-$path = Join-Path $pwd "karma"
-Start-Process -FilePath $path -ArgumentList "start"
+$path = Join-Path $nodePath "node.exe"
+Start-Process -FilePath $path -WorkingDirectory $nodePath -ArgumentList "karma", "start"
 "Sleeping for 30 seconds to let karma server get fully started"
 Start-Sleep -s 30
 $karmaProcId = Get-Process -Name "node" | select -expand id
 "karmaProcId is $karmaProcId"
-"Starting firefox"
-$exePath = "C:\Program Files\Mozilla Firefox"
-$exeName = "firefox.exe"
-$path = Join-Path $exePath $exeName
-Start-Process -FilePath $path -ArgumentList "http://localhost:8000" "-headless"
-if ($? -ne $true) {
-    "Could not start firefox"
+if ($karmaProcId) {
+    "Starting firefox"
+    $exePath = "C:\Program Files\Mozilla Firefox"
+    $exeName = "firefox.exe"
+    $path = Join-Path $exePath $exeName
+    Start-Process -FilePath $path -ArgumentList "http://localhost:8000" "-headless"
+    if ($? -ne $true) {
+        "Could not start firefox"
+    }
+    $procId = Get-Process -Name "firefox" | select -expand id
+    "firefox PID is $procId"
+} else {
+    "Karma did not start. Did not start firefox."
 }
-$procId = Get-Process -Name "firefox" | select -expand id
-"firefox PID is $procId"
+
