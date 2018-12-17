04ed6ad1070988e43b5184f0da60ad0eda894021
Jill Stellman <techgirl@mail.com>
2018-12-17 14:36:20 -0500

    Added more debug messages different places to try to identify why firefox isn't connecting to/being captured by karma.

--- startFirefox.ps1
+++ startFirefox.ps1
@@ -6,17 +6,21 @@ $path = Join-Path $nodePath "node.exe"
 "path to node is $path"
 "working directory is $pwd"
 $karmaFFPath = Join-Path $pwd "node_modules\karma-firefox-launcher"
-$karmaCapturePath = Join-Path $pwd "node_modules\karma\lib\launchers"
 $karmaServerPath = Join-Path $pwd "node_modules\karma\lib"
+$karmaHtml2JsPath = Join-Path $pwd "node_modules\karma-html2js-preprocessor\lib"
+$karmaConnectPath = Join-Path 4pwd "node_modules\connect"
 $subFile = Join-Path $pwd "subs/index.js"
-$capTOFile = Join-Path $pwd "subs/capture_timeout.js"
 $serverFile = Join-Path $pwd "subs/server.js"
+$html2JsFile = Join-Path $pwd "subs/html2js.js"
+$connectFile = Join-Path $pwd "subs/connect/index.js"
 "Copying $subFile to $karmaFFPath"
 Copy-Item $subFile -Destination $karmaFFPath
-"Copying $capTOFile to $karmaCapturePath"
-Copy-Item $capTOFile -Destination $karmaCapturePath
 "Copying $serverFile to $karmaServerPath"
 Copy-Item $serverFile -Destination $karmaServerPath
+"Copying $html2JsFile to $karmaHtml2JsPath"
+Copy-Item $html2JsFile -Destination $karmaHtml2JsPath
+"Copying $connectFile to $karmaConnectPath"
+Copy-Item $connectFile -Destination $karmaConnectPath
 # Start-Process -FilePath $path -WorkingDirectory $nodePath -ArgumentList "karma", "start"
 # "Sleeping for 30 seconds to let karma server get fully started"
 # Start-Sleep -s 30
